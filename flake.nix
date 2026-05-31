40e854b5e6731e3a44b85194bbb92338a7f331c9
owo93 <iztyle30@gmail.com>
2026-05-31 14:14:07 +0700

    refactor: switch to import-tree

--- flake.nix
+++ flake.nix
@@ -1,22 +1,21 @@
 {
-  description = "My neovim config";
+  description = "My nvf neovim configuration";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    haumea.url = "github:nix-community/haumea";
+    import-tree.url = "github:denful/import-tree";
     nvf.url = "github:notashelf/nvf";
   };
 
   outputs =
-    inputs@{
+    {
       flake-parts,
+      import-tree,
       nvf,
-      haumea,
       ...
-    }:
+    }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
-
       systems = [
         "x86_64-linux"
         "aarch64-linux"
@@ -24,57 +23,49 @@
         "aarch64-darwin"
       ];
 
+      flake = {
+        homeModules.default =
+          { pkgs, ... }:
+          {
+            home.packages = [
+              (nvf.lib.neovimConfiguration {
+                inherit pkgs;
+                modules = [ (import-tree ./modules) ];
+              }).neovim
+            ];
+          };
+      };
+
       perSystem =
-        { pkgs, ... }:
+        { system, ... }:
         let
-          moduleTree = haumea.lib.load {
-            src = ./modules;
+          pkgs = import inputs.nixpkgs {
+            inherit system;
           };
 
-          collectModules =
-            value:
-            if builtins.isAttrs value then
-              if value ? config then [ value ] else builtins.concatMap collectModules (builtins.attrValues value)
-            else
-              [ ];
+          nvimConfig = nvf.lib.neovimConfiguration {
+            inherit pkgs;
+            modules = [ (import-tree ./modules) ];
+          };
         in
         {
-          packages.default =
-            (nvf.lib.neovimConfiguration {
-              inherit pkgs;
+          packages.default = nvimConfig.neovim;
 
-              modules = [
-                {
-                  config.vim = {
-                    theme = {
-                      enable = true;
-                      name = "solarized-osaka";
-                      style = "dark";
-                      transparent = true;
-                    };
-                    globals = {
-                      mapleader = " ";
-                    };
-                    lsp = {
-                      enable = true;
-                      formatOnSave = true;
-                    };
+          apps.default = {
+            type = "app";
+            program = "${nvimConfig.neovim}/bin/nvim";
+            meta = {
+              description = "launch nvf config";
+            };
+          };
 
-                    statusline.lualine = {
-                      enable = true;
-                      theme = "solarized_dark";
-                    };
-
-                    autocomplete.blink-cmp.enable = true;
-                    telescope.enable = true;
-                    binds.whichKey.enable = true;
-                    vendoredKeymaps.enable = true;
-                  };
-                }
-              ]
-              ++ collectModules moduleTree;
-            }).neovim;
+          formatter = pkgs.nixfmt;
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              nixfmt
+              nixd
+            ];
+          };
         };
     };
-
 }
--- modules/ui.nix
+++
@@ -1,3 +0,0 @@
-{
-  config.vim.ui.borders.enable = true;
-}
