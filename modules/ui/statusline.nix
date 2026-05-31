5b900a095ecc70ff8741633fbee3a7d189b82852
owo93 <iztyle30@gmail.com>
2026-05-31 16:43:57 +0700

    style: prettify lualine

--- modules/ui/statusline.nix
+++ modules/ui/statusline.nix
@@ -3,6 +3,23 @@
     statusline = {
       lualine = {
         enable = true;
+        theme = "auto";
+        componentSeparator.left = "";
+        componentSeparator.right = "";
+        sectionSeparator = {
+          left = "";
+          right = "";
+        };
+        activeSection = {
+          a = [
+            ''{ "mode", separator = { left = "", right = "" }, right_padding = 4 }''
+          ];
+          b = [ ];
+          y = [ "lsp_status" ];
+          z = [
+            ''{ "filetype", separator = { left = "", right = "" }, left_padding = 2 }''
+          ];
+        };
       };
     };
   };
