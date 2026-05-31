0ee40e798ee1c872c093411c2df97ca5b7eeb54d
owo93 <iztyle30@gmail.com>
2026-05-31 19:02:26 +0700

    refactor: use noice setupOpts

--- modules/ui/noice.nix
+++ modules/ui/noice.nix
@@ -1,45 +1,35 @@
-{ pkgs, ... }:
 {
   vim = {
-    startPlugins = [
-      pkgs.vimPlugins.noice-nvim
-      pkgs.vimPlugins.nui-nvim
-      pkgs.vimPlugins.nvim-notify
-    ];
-
-    pluginRC.nvim-notify = ''
-      require("notify").setup({
-        background_colour = "#000000",
-        stages = "fade_in_slide_out",
-        timeout = 1000
-      })
-    '';
-
-    pluginRC.noice-nvim = ''
-      local noice = require("noice")
-      noice.setup({
+    ui.noice = {
+      enable = true;
+      setupOpts = {
         lsp = {
-          progress = { enabled = true },
+          progress.enabled = true;
           override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true,
-          },
-        },
+            "vim.lsp.util.convert_input_to_markdown_lines" = true;
+            "vim.lsp.util.stylize_markdown" = true;
+            "cmp.entry.get_documentation" = true;
+          };
+        };
         presets = {
-          bottom_search = true,
-          command_palette = true,
-          long_message_to_split = true,
-        },
+          bottom_search = true;
+          command_palette = true;
+          long_message_to_split = true;
+        };
         views = {
-          cmdline_popup = {
-            enabled = false,
-          },
-          popupmenu = {
-            enabled = false,
-          },
-        },
-      })
-    '';
+          cmdline_popup.enabled = false;
+          popupmenu.enabled = false;
+        };
+      };
+    };
+
+    notify.nvim-notify = {
+      enable = true;
+      setupOpts = {
+        background_colour = "#000000";
+        stages = "fade_in_slide_out";
+        timeout = 1000;
+      };
+    };
   };
 }
