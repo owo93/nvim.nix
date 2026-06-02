2ae5760f680b1d43de2c68f24e05b7efeddd5374
owo93 <iztyle30@gmail.com>
2026-06-02 11:11:07 +0700

    fix: switch incline-nvim to extraPlugins

--- modules/ui/incline.nix
+++ modules/ui/incline.nix
@@ -1,52 +1,53 @@
 { pkgs, ... }:
 {
   vim = {
-    startPlugins = [
-      pkgs.vimPlugins.incline-nvim
-    ];
+    extraPlugins = {
+      incline = {
+        package = pkgs.vimPlugins.incline-nvim;
+        setup = ''
+          local helpers = require("incline.helpers")
+          local devicons = require('nvim-web-devicons')
+          local colors = require('solarized-osaka.colors').setup()
 
-    pluginRC.incline = ''
-      local helpers = require("incline.helpers")
-      local devicons = require('nvim-web-devicons')
-      local colors = require('solarized-osaka.colors').setup()
+          require('incline').setup({
+            highlight = {
+              groups = {
+                InclineNormal = { guifg = colors.violet500 },
+                InclineNormalNC = { guifg = colors.violet600, guibg = colors.base03 },
+              },
+            },
+            window = {
+              padding = 0,
+              margin = { horizontal = 1, vertical = 0 },
+            },
 
-      require('incline').setup({
-        highlight = {
-          groups = {
-            InclineNormal = { guifg = colors.violet500 },
-            InclineNormalNC = { guifg = colors.violet600, guibg = colors.base03 },
-          },
-        },
-        window = {
-          padding = 0,
-          margin = { horizontal = 1, vertical = 0 },
-        },
+            render = function(props)
+              local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+              if filename == "" then
+                filename = "[No Name]"
+              end
+              
+              local ft_icon, ft_color = devicons.get_icon_color(filename)
+              local modified = vim.bo[props.buf].modified
+              
+              local display_filename = filename
+              if modified then
+                display_filename = "[+] " .. filename
+              end
 
-        render = function(props)
-          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-          if filename == "" then
-            filename = "[No Name]"
-          end
-          
-          local ft_icon, ft_color = devicons.get_icon_color(filename)
-          local modified = vim.bo[props.buf].modified
-          
-          local display_filename = filename
-          if modified then
-            display_filename = "[+] " .. filename
-          end
+              local icon_bg = ft_color or colors.magenta600 or "#e80045"
+              local icon_fg = helpers.contrast_color(icon_bg)          
 
-          local icon_bg = ft_color or colors.magenta600 or "#e80045"
-          local icon_fg = helpers.contrast_color(icon_bg)          
-
-          return {
-            ft_icon and { " ", ft_icon, " ", guibg = icon_bg, guifg = icon_fg } or " ",
-            { " ", guibg = colors.base04 },
-            { display_filename, gui = "bold", guibg = colors.base04 },
-            { " ", guibg = colors.base04 },
-          }
-        end,
-      })
-    '';
+              return {
+                ft_icon and { " ", ft_icon, " ", guibg = icon_bg, guifg = icon_fg } or " ",
+                { " ", guibg = colors.base04 },
+                { display_filename, gui = "bold", guibg = colors.base04 },
+                { " ", guibg = colors.base04 },
+              }
+            end,
+          })
+        '';
+      };
+    };
   };
 }
